665edfbba2847d6e69e53109d23747e05a0265d4
Rookie-rar <3213817196@qq.com>
2024-12-06 19:07:00 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return "hello world"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from http.server import BaseHTTPRequestHandler
+ 
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write("DearXuan's API by python!".encode())
+        return
